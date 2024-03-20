3ff16c028de7a11175c98b2ba33a0bd7ba87838d
Oghenero-Sigbenu <sigbenuoghenero@github.com>
2024-03-20 14:48:33 +0100

    updates

--- src/Components/AboutUs/Vision.jsx
+++ src/Components/AboutUs/Vision.jsx
@@ -4,27 +4,27 @@ import Target from "../../Assests/target.png";
 function Vision() {
   return (
     <div className="w-full  bg-[rgba(142,224,254,0.29)] pt-[3rem] pb-[3rem]">
-      <div className="w-full mb-[4rem]">
-        <img
-          src={VisionIcon}
-          alt="icon"
-          className="w-[80px] mx-auto mb-[1rem]"
-        />
+      <div className="flex flex-wrap ">
+        <div className="w-full md:w-[48%] mb-[4rem]">
+          <img
+            src={VisionIcon}
+            alt="icon"
+            className="w-[80px] mx-auto mb-[1rem]"
+          />
 
-        <h4 className="text-[2rem] text-center  font-extrabold leading-8 text-primary ">
-          Our Vision
-        </h4>
-        <p className="mt-[1rem] md:w-[80%] mx-auto text-center  text-[1rem] leading-7">
-          To create a world where healthcare is a fundamental right not a
-          privilege.
-          <br />
-          Our vision is to create a world where every individual, regardless of
-          their location or socio-economic status, has equitable access to
-          high-quality healthcare services. We envision vibrant and healthy
-          communities thriving in even the most remote corners of our planet.
-        </p>
-      </div>
-      <div className="flex justify-between flex-wrap  mt-[3rem] max-w-[90%] lg:max-w-[85%] mx-auto ">
+          <h4 className="text-[2rem] text-center  font-extrabold leading-8 text-primary ">
+            Our Vision
+          </h4>
+          <p className="mt-[1rem] md:w-[80%] mx-auto text-center  text-[1rem] leading-7">
+            To create a world where healthcare is a fundamental right not a
+            privilege.
+            <br />
+            Our vision is to create a world where every individual, regardless
+            of their location or socio-economic status, has equitable access to
+            high-quality healthcare services. We envision vibrant and healthy
+            communities thriving in even the most remote corners of our planet.
+          </p>
+        </div>
         <div className="w-full md:w-[48%] mb-[4rem]">
           <img
             src={VisionIcon}
@@ -35,14 +35,24 @@ function Vision() {
           <h4 className="text-[2rem] text-center  font-extrabold leading-8 text-primary ">
             Our Mission
           </h4>
-          {/* <p className="mt-[1rem] text-center  text-[1rem] leading-7">
+          <p className="mt-[1rem] md:w-[80%] mx-auto text-center  text-[1rem] leading-7">
             Our mission is to provide free,accessible, and essential healthcare
             services to underserved populations in rural areas, students, and
             marginalized communities. Through innovative approaches and
             community partnerships, we aim to improve health outcomes, reduce
-            disparities, and empower individuals to lead healthier lives by
-            theres processes
-          </p> */}
+            disparities, and empower individuals to lead healthier lives.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-between flex-wrap  mt-[3rem] max-w-[90%] lg:max-w-[85%] mx-auto ">
+        <div className="w-full md:w-[48%] mb-[4rem]">
+          <img src={Target} alt="icon" className="w-[80px] mx-auto mb-[1rem]" />
+
+          <h4 className="text-[2rem] text-center  font-extrabold leading-8 text-primary ">
+            What we do?
+          </h4>
+
           <ul className="text-left">
             <li className="mb-[1rem]">
               {" "}
